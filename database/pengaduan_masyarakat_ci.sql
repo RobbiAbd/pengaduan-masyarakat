5b7c1e1fd4389030fec9080d9106a6a6f60df3dc
Estu Fardani <andro.medh4@gmail.com>
2023-06-04 17:40:47 +0700

    reimport db from web

--- database/pengaduan_masyarakat_ci.sql
+++ database/pengaduan_masyarakat_ci.sql
@@ -60,7 +60,8 @@ CREATE TABLE `masyarakat` (
   `telp` varchar(13) NOT NULL,
   `alamat` varchar(35) NOT NULL,
   `foto_profile` varchar(225) NOT NULL,
-  `is_verified` tinyint(1) NOT NULL DEFAULT '0'
+  `is_verified` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`nik`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -68,7 +69,8 @@ CREATE TABLE `masyarakat` (
 --
 
 INSERT INTO `masyarakat` (`nik`, `nama`, `username`, `password`, `telp`, `alamat`, `foto_profile`, `is_verified`) VALUES
-(1212345678912354, 'aisyah', 'masyarakat', '$2y$10$BqCVWU56ME/Y.MctVXBw7uI8w26d1gK/HY219JiQWe./ppfYVEeYS', '08131111111', 'pangkal', 'user.png', 1);
+(12345678918,'lulu','lululala','$2y$10$J23NNXSjscUHCEHXDkSaTOvbm8gQYRVmMtdqCGPQyJuFeuMfS.hJG','08111111111','PKG','user.png',1),
+(1212345678912354,'aisyah','masyarakat','$2y$10$BqCVWU56ME/Y.MctVXBw7uI8w26d1gK/HY219JiQWe./ppfYVEeYS','08131111111','pangkal','user.png',1);
 
 -- --------------------------------------------------------
 
@@ -88,9 +90,21 @@ CREATE TABLE `pengaduan` (
   `isi_laporan` text NOT NULL,
   `foto` varchar(255) NOT NULL,
   `status` enum('Diajukan','Diproses','Selesai','Ditolak') NOT NULL,
-  `id_kabupaten` int(11) NOT NULL
+  `id_kabupaten` int(11) NOT NULL,
+  PRIMARY KEY (`id_pengaduan`),
+  KEY `nik` (`nik`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `pengaduan`
+--
+
+LOCK TABLES `pengaduan` WRITE;
+/*!40000 ALTER TABLE `pengaduan` DISABLE KEYS */;
+INSERT INTO `pengaduan` VALUES
+(12,'2023-01-26',1212345678912354,'Sodara','Amir','Palembang','Riri','Pelecehan Seksual','Pelaku memukul korban menggunakan tauge','f158381b96b131e019fd1d6f3d9da57e.jpg','Diproses',7),
+(15,'2023-01-26',1212345678912354,'kawan','Tedjo','bekasi','suasana','Kekerasan Dalam Rumah Tangga','lupa makan','c84e4069757743fa8f35c29a74c0d2b2.jpg','Diproses',6),
+(16,'2023-02-14',1212345678912354,'Teman','Budi','Pantai','Raya','Kekerasan Dalam Rumah Tangga','Tidak memberikan izin tinggal','d8695e60c4c69842e4209cbde61a4ced.jpg','Diajukan',5);
 -- --------------------------------------------------------
 
 --
@@ -106,7 +120,9 @@ CREATE TABLE `petugas` (
   `telp` varchar(13) NOT NULL,
   `alamat` varchar(35) NOT NULL,
   `level` enum('admin','petugas') NOT NULL,
-  `foto_profile` varchar(225) NOT NULL
+  `foto_profile` varchar(225) NOT NULL,
+  PRIMARY KEY (`id_petugas`),
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -126,7 +142,11 @@ INSERT INTO `petugas` (`id_petugas`, `nama`, `nik`, `username`, `password`, `tel
 CREATE TABLE `petugas_kabupaten` (
   `id` int(11) NOT NULL,
   `petugas_id` int(11) NOT NULL,
-  `kabupaten_id` int(11) NOT NULL
+  `kabupaten_id` int(11) NOT NULL,
+  KEY `petugas_kabupaten_ibfk_1` (`petugas_id`),
+  KEY `petugas_kabupaten_ibfk_2` (`kabupaten_id`),
+  CONSTRAINT `petugas_kabupaten_ibfk_1` FOREIGN KEY (`petugas_id`) REFERENCES `petugas` (`id_petugas`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `petugas_kabupaten_ibfk_2` FOREIGN KEY (`kabupaten_id`) REFERENCES `kabupaten` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -148,107 +168,27 @@ CREATE TABLE `tanggapan` (
   `id_pengaduan` bigint(16) NOT NULL,
   `tgl_tanggapan` date NOT NULL,
   `tanggapan` text NOT NULL,
-  `id_petugas` int(11) NOT NULL
+  `id_petugas` int(11) NOT NULL,
+  PRIMARY KEY (`id_tanggapan`),
+  KEY `id_pengaduan` (`id_pengaduan`),
+  KEY `id_petugas` (`id_petugas`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `tanggapan`
 --
 
---
--- Indexes for table `kabupaten`
---
-ALTER TABLE `kabupaten`
-  ADD PRIMARY KEY (`id`);
+/*!40000 ALTER TABLE `tanggapan` DISABLE KEYS */;
+INSERT INTO `tanggapan` VALUES (19,10,'2023-01-11','oke konfirm',6),(20,15,'2023-02-14','sedang didalami',6),(21,12,'2023-02-14','Sedang dalam proses',6);
 
---
--- Indexes for table `masyarakat`
---
-ALTER TABLE `masyarakat`
-  ADD PRIMARY KEY (`nik`);
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for table `pengaduan`
---
-ALTER TABLE `pengaduan`
-  ADD PRIMARY KEY (`id_pengaduan`),
-  ADD KEY `nik` (`nik`);
-
---
--- Indexes for table `petugas`
---
-ALTER TABLE `petugas`
-  ADD PRIMARY KEY (`id_petugas`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- Indexes for table `petugas_kabupaten`
---
-ALTER TABLE `petugas_kabupaten`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `petugas` (`petugas_id`),
-  ADD KEY `kabupaten` (`kabupaten_id`);
-
---
--- Indexes for table `tanggapan`
---
-ALTER TABLE `tanggapan`
-  ADD PRIMARY KEY (`id_tanggapan`),
-  ADD KEY `id_pengaduan` (`id_pengaduan`),
-  ADD KEY `id_petugas` (`id_petugas`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `kabupaten`
---
-ALTER TABLE `kabupaten`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `pengaduan`
---
-ALTER TABLE `pengaduan`
-  MODIFY `id_pengaduan` bigint(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `petugas`
---
-ALTER TABLE `petugas`
-  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `petugas_kabupaten`
---
-ALTER TABLE `petugas_kabupaten`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `tanggapan`
---
-ALTER TABLE `tanggapan`
-  MODIFY `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `pengaduan`
---
-ALTER TABLE `pengaduan`
-  ADD CONSTRAINT `pengaduan_ibfk_1` FOREIGN KEY (`id_kabupaten`) REFERENCES `kabupaten` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `petugas_kabupaten`
---
-ALTER TABLE `petugas_kabupaten`
-  ADD CONSTRAINT `petugas_kabupaten_ibfk_1` FOREIGN KEY (`petugas_id`) REFERENCES `petugas` (`id_petugas`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `petugas_kabupaten_ibfk_2` FOREIGN KEY (`kabupaten_id`) REFERENCES `kabupaten` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-04 10:29:03
