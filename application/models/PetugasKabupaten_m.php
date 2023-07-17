fb1193e3060a38f626cba13027767dfcca8f929f
MHafizAF <hafiz170301@gmail.com>
2023-07-17 20:17:33 +0700

    change column id in table petugas_kabupaten to id_petugaskab

--- application/models/PetugasKabupaten_m.php
+++ application/models/PetugasKabupaten_m.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class PetugasKabupaten_m extends CI_Model {
 
 	private $table = 'petugas_kabupaten';
-	private $primary_key = 'id';
+	private $primary_key = 'id_petugaskab';
 
 	public function create($data)
 	{
